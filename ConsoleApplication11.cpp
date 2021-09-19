9d65393e8ef5b3f73b739929f807e4932030e132
Kim <kim.zyong@yandex.ru>
2021-09-19 12:40:57 +0300

    Lesson 1

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> //подключение библиотеки
+using namespace std; //magic
+
+
+int main()
+{
+    double v; //создание переменной
+    const double g = 9.81; //создание константы
+
+    cout << "Input v>>";
+    cin >> v;
+    double T = 2*v/g;
+    double h = v*v/2/g;
+
+    cout << "T = " << T << "\nh = " << h << '\n'; //\n - переход на следующую строку
+    /*
+    * "" - строка
+    * '' - символ
+    */
+}
+
